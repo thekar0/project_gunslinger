f7f836d448083d0527eff592fce8254c55574c7f
thekar0 <okar2007ka@gmail.com>
2025-02-14 12:45:20 +0100

    Endless mapa i Basic enemy

---
+++ scenes/enemies/enemy_1.gd
@@ -0,0 +1,26 @@
+extends CharacterBody3D
+
+@onready var player = null
+
+const SPEED = 6.0
+var health = 200
+
+@export var player_path : NodePath
+
+@onready var nav_agent = $NavigationAgent3D
+
+func _ready() -> void:
+	player = get_node(player_path)
+	
+func _process(delta: float) -> void:
+	if player != null:
+		velocity = Vector3.ZERO
+		nav_agent.set_target_position(player.global_transform.origin)
+		var next_nav_point = nav_agent.get_next_path_position()
+		velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+		move_and_slide()
+
+func damage():
+	health-=20
+	if health<=0:
+		queue_free()
